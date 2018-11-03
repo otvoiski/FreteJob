89af4b4455c64080a79c5a707c450b8f69dfdca2
Otávio Voiski <otvoiski@live.com>
2018-11-03 20:04:34 -0300

    Adicionado Pessoa DAO e Controller Pessoa Fisica

---
+++ src/Controller/PessoaFisicaController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Model.PessoaFisica;
+
+/**
+ *
+ * @author Otavio
+ */
+public class PessoaFisicaController extends ControllerBase{
+
+    @Override
+    protected void START_CONTROLLER() {
+        DAO = new DAO.PessoaFisicaDAO(Model.PessoaFisica.class);
+        Object = new PessoaFisica();
+    }
+    
+}
---
+++ src/DAO/PessoaFisicaDAO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+/**
+ *
+ * @author Otavio
+ */
+public class PessoaFisicaDAO extends Persistencia {
+     
+    public PessoaFisicaDAO(Class persistedClass) {
+        super(persistedClass);
+    }
+     
+}
