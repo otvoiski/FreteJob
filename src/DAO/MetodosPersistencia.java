3a3bd482a6901a71888e2d9dd97afe2c1a842a20
Otávio Voiski <otvoiski@live.com>
2018-11-04 18:04:28 -0200

    Metodos Persistencia Ok, Agora é possivel realizar um CRUD ao banco de dados com espelhamentos de qual quer classe.

--- src/DAO/MetodosPersistencia.java
+++ src/DAO/MetodosPersistencia.java
@@ -24,10 +24,16 @@ import java.util.logging.Logger;
  */
 public class MetodosPersistencia {
 
-    public static boolean Query(String query) {
+    public static boolean Query(String query, String[] array) {
         Connection conn = FabricaConexao.GetTransaction();
         try {            
             PreparedStatement ps = conn.prepareStatement(query);
+            
+            // Evita o SQL Inject
+            for (int i = 0; i < array.length; i++) {
+                ps.setString(i+1, array[i]);
+            }
+            
             return FabricaConexao.CommitTransaction(conn, (ps.executeUpdate() > 0));
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
@@ -36,6 +42,7 @@ public class MetodosPersistencia {
             FabricaConexao.CloseConnection(conn);
         }
     }
+    
     private static ObjectBase SetObject(Class classe, ResultSet rs){
         try {             
             Constructor ctor = classe.getDeclaredConstructor(ResultSet.class);
@@ -48,9 +55,9 @@ public class MetodosPersistencia {
         return null;
     }
     
-   public static String getValue(Field field, Object obj) {
-       field.setAccessible(true);
-        try {
+    public static String getValue(Field field, Object obj) {
+       try { 
+            field.setAccessible(true);
             return (String) field.get(obj);
         } catch (IllegalArgumentException | IllegalAccessException ex) {
             Logger.getLogger(MetodosPersistencia.class.getName()).log(Level.SEVERE, null, ex);    
@@ -64,52 +71,76 @@ public class MetodosPersistencia {
      * @param obj 
      * @return  
      */
-    public static boolean persistir(Object obj) {
-        /*
-        String query = "Insert into " + obj.getClass().getSimpleName() + " ( ";
-        for (int i = 0; i < obj.getClass().getDeclaredFields().length; i++) {
-            query += obj.getClass().getDeclaredFields()[i].getName();
-            if(i < obj.getClass().getDeclaredFields().length-1)
+    public static boolean persistir(Object obj) {        
+        int length = obj.getClass().getDeclaredFields().length;
+        
+        String query = "insert into " + obj.getClass().getSimpleName() + " ( ";
+        for (int i = 1; i < length; i++) { //i = 1; Como iremos persistir o obj, não precisaremos do codigo!
+            String name = obj.getClass().getDeclaredFields()[i].getName();             
+            query += name;
+            if(i < length-1)
                 query += ", ";
         }
         query += " ) values ( ";
-        for (int i = 0; i < obj.getClass().getDeclaredFields().length; i++) {
-            try {
-                query += "'" + getValue(obj.getClass().getDeclaredFields()[i], obj.getClass()) + "'";
-            } catch (SecurityException ex) {
-                Logger.getLogger(MetodosPersistencia.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            if(i < obj.getClass().getDeclaredFields().length-1)
-                query += ", ";
-        }
         
+        String[] datas = new String[length-1]; //-1 significa que estarei tirando o atributo Codigo!
+        try {
+            for (int i = 0; i < datas.length; i++) {
+                Field field = obj.getClass().getDeclaredField(   obj.getClass().getDeclaredFields()[i+1].getName()   );
+                query += "?";
+                datas[i] = getValue(field, obj);                
+                if(i < datas.length-1)
+                    query += ", ";          
+            }         
+        } catch (NoSuchFieldException ex) {
+            Logger.getLogger(MetodosPersistencia.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            Logger.getLogger(MetodosPersistencia.class.getName()).log(Level.SEVERE, null, ex);
+          
+        }        
         query += " )";
-        */
-        String query = "Insert into " + obj.getClass().getSimpleName() + " ( ";
-        for (int i = 0; i < obj.getClass().getDeclaredFields().length; i++) {
-            query += obj.getClass().getDeclaredFields()[i].getName();
-            if(i < obj.getClass().getDeclaredFields().length-1)
-                query += ", ";
-        }
-        query += " ) values ( ?, ? )";      
         
-        return Query(query);
+        return Query(query, datas);
     }
     
     /**
      * Update
      * @param obj 
      */
-    public static void fundir(Object obj) {
+    public static boolean fundir(Object obj) {        
+        int length = obj.getClass().getDeclaredFields().length;
+        String query = "update " + obj.getClass().getSimpleName() + " set ";
         
+        for (int i = 1; i < length; i++) { //i = 1; Como iremos persistir o obj, não precisaremos do codigo!
+            String name = obj.getClass().getDeclaredFields()[i].getName();             
+            query += name + " = ?";
+            if(i < length-1)
+                query += ", ";
+        }
+        
+        query += " where Codigo = ?";        
+        
+        String[] datas = new String[length];
+        try {
+            for (int i = 0; i < datas.length-1; i++) {
+                Field field = obj.getClass().getDeclaredField(   obj.getClass().getDeclaredFields()[i+1].getName()   );
+                datas[i] = getValue(field, obj);
+            }  
+            datas[length-1] = getValue(obj.getClass().getDeclaredField(   obj.getClass().getDeclaredFields()[0].getName()   ), obj);
+        } catch (NoSuchFieldException ex) {
+            Logger.getLogger(MetodosPersistencia.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            Logger.getLogger(MetodosPersistencia.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        
+        return Query(query, datas);
     }
     
     public static boolean excluir(String chave, Class classe) throws SQLException{
         String query = "delete from " + classe.getSimpleName() + " where Codigo = " + chave;        
-        return Query(query);        
+        return Query(query, (new String[]{chave}));        
     }
-    
-    
+  
     public static ObjectBase recuperar(String chave, Class classe) throws SQLException{
         Connection conexao = FabricaConexao.NewSingleton();
         String query = "select * from " + classe.getSimpleName() + " where Codigo = " + chave;        
