3570f1d82f32ce889b3d6d2d3ccf7d5c148bd03f
Otávio Voiski <otvoiski@live.com>
2018-11-06 23:52:25 -0200

    Classe de teste

--- src/View/FreteJob.java
+++ src/View/FreteJob.java
@@ -98,9 +98,15 @@ public class FreteJob {
         /*JSONObject json = (new Controller.PessoaFisicaController()).Get("0");
         System.err.println(json);*/
         
-        //JSONObject cliente = (new Controller.ClienteController()).Get("2");
-        //System.out.println(cliente);
+        JSONObject cliente = (new Controller.ClienteController()).Get("2");
+        System.out.println(cliente);
+        
+        /*
         ArrayList<JSONObject> clientes = (new Controller.ClienteController()).GetAll();
+        for (JSONObject c : clientes) {
+            System.out.println(c);
+        }
+        */
     }
     
 }
