c0f5c65ef81e95236287d58af10729d22db7c2dc
Otávio Voiski <otvoiski@live.com>
2018-11-15 19:58:45 -0200

    Persistencias Usuario, Pais

---
+++ src/DAO/UsuarioDAO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+/**
+ *
+ * @author Otavio
+ */
+public class UsuarioDAO extends Base.Persistencia{
+    
+    public UsuarioDAO(Class persistedClass) {
+        super(persistedClass);
+    }
+    
+}
