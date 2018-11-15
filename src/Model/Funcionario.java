00b4e30f2c628feab696701fe2c639823c7613e7
Otávio Voiski <otvoiski@live.com>
2018-11-15 19:58:24 -0200

    libs importadas que não estavam sendo utilizadas foram deletas.

--- src/Model/Funcionario.java
+++ src/Model/Funcionario.java
@@ -6,9 +6,7 @@
 package Model;
 
 import Base.ObjectBase;
-import java.sql.ResultSet;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import org.json.JSONObject;
 
