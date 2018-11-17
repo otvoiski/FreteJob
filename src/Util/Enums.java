77c52d7e93a3b5427babdec892eaf5e8cb87fba0
Otávio Voiski <otvoiski@live.com>
2018-11-17 21:35:34 -0200

    Criação da classe Enums

---
+++ src/Util/Enums.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Util;
+
+/**
+ *
+ * @author Otavio
+ */
+public class Enums {    
+    public static enum TipoTransporte {TERRESTRES, AQUATICOS, AEROVIARIOS}
+    public static enum TipoPessoa {Juridica, Fisica}
+}
