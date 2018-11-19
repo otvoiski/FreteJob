7baa106d20dc7dc3fa1b7f71b9198afcf5e530ee
Otávio Voiski <otvoiski@live.com>
2018-11-19 00:10:00 -0200

    Alteração no formato de JOINED

--- src/Base/ObjectBase.java
+++ src/Base/ObjectBase.java
@@ -18,7 +18,7 @@ import org.json.JSONObject;
  * @author Aluno
  */
 @MappedSuperclass
-@Inheritance(strategy = InheritanceType. TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class ObjectBase {
     @Id
     @GeneratedValue(strategy = GenerationType.TABLE)
